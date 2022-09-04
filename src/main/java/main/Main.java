c7cde2f719028030f51c209b0e6d8b39b0e03d32
Jardel da Silva <jardel.silva0206@gmail.com>
2022-09-04 20:53:24 -0300

    Inicio da construção da interface

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package TodoAPP;
+package main;
 
 import controller.ProjectController;
 import java.sql.Date;
